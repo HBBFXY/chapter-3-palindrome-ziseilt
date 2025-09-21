3484ec5d847ce0ea9326b035da7e28179c22ecb3
ziseilt <1157468223@qq.com>
2025-09-21 11:15:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 # 这里编写你的代码
+num = input("请输入一个5位数：")
+if len(num) != 5:
+    print("输入错误，请确保输入的是5位数！")
+else:
+    if num == num[::-1]:
+        print("{} 是回文数".format(num))
+    else:
+        print("{} 不是回文数".format(num))
